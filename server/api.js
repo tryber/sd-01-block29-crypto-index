cdcb840e4a2b3cb1b38e2b5b956b6b19bbb7f570
vrramosm <vf.ramos7@gmail.com>
2020-04-28 15:11:58 -0300

    Iniciando Projeto | Massa!

---
+++ server/api.js
@@ -0,0 +1,17 @@
+import useAxios from 'axios-hooks';
+
+function getAPI() {
+  const [{ data, loading, error }, refetch] = useAxios('https://api.coindesk.com/v1/bpi/currentprice.json');
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
+  return (
+    <div>
+      <button onClick={refetch}>Atualizar</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default getAPI;
