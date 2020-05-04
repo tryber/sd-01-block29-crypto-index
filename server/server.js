c46f6168db0bc5caeddaa2f49c20e2cb786a4e3b
antoniopaolillo <antonio_francisco_p@live.com>
2020-05-04 11:42:45 -0300

    commitando p jogar na master

--- server/server.js
+++ server/server.js
@@ -1,8 +1,28 @@
-require('ignore-styles')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-require('@babel/register')({
-  ignore: [/(node_module)/],
-  presets: ['@babel/preset-env', '@babel/preset-react']
-})
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 
-require('./server')
+import Login from '../src/Login.js';
+
+const app = express();
+
+const reactApp = ReactDOMServer.renderToString(<Login />);
+
+app.use('^/login$', (req, res, next) => {
+  fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Algo deu errado :(');
+    }
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+    );
+  });
+});
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
