3a55fa895fe92e0ab557f8761f67ee33327cdc3f
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-05-07 19:34:45 -0300

    concertando linhas aqui

--- api/home.js
+++ api/home.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',  (req, res) => {
+router.get('/', (undefined, res) => {
   res.send('aqui começa tudo!');
 });
 
