4bc4119457355f152613875aa3a59d4ff9d89fc5
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-05-11 20:18:23 -0300

    Terminando o get. Agora tenho que fazer o post

--- api/cryto/btc.js
+++ api/cryto/btc.js
@@ -7,18 +7,19 @@ const router = express.Router();
 const fs = require('fs');
 
 const fileName = 'currencies.json';
-let read =''
- fs.readFile(fileName, 'utf8', (err, data) => {
+
+const formatNumber = require('../../service/converter')
+
+let read = '';
+
+fs.readFile(fileName, 'utf8', (err, data) => {
   if (err) {
     console.error(`Não foi possível ler o arquivo ${fileName}\n Erro: ${err}`);
     process.exit(1);
   }
   const json = JSON.parse(data);
-  console.log('aqui tem alguma coisa', json);
   read = json;
 });
-
-// minha função Read não ta funfando
 
 const URL = (currency = 'currentprice.json') =>
   `https://api.coindesk.com/v1/bpi/${currency}`;
@@ -26,54 +27,44 @@ const URL = (currency = 'currentprice.json') =>
 const getSomeData = () =>
   axios
     .get(`${URL()}`)
-    .then(({ data }) => data.bpi)
+    .then(({ data }) => data)
     .catch((err) => console.error(err));
 
-// 1 BTC em dolares = 6,506.6717 dólares (campo rate_float de USD no resultado da API)
+const creatorObject = (code, rate, description) => {
+  const floatRate = parseF(rate, 4);
+  const floatRateString = formatNumber(rate);
+  return {
+    code,
+    symbol: '&#36;',
+    rate: floatRateString,
+    description,
+    rate_float: floatRate,
+  };
+};
 
-// 1 BTC em reais = 5,40 (rate_float de BRL) * 6,506.6717 (rate_float de USD) = 35,136.02718 reais.
+const parseF = (value, length) => Number(parseFloat(value).toFixed(length));
 
-router.get('/cryto/btc', async (req, res) => {
-  const dataFetch = await getSomeData();
-  const {BRL} =  read;
-  console.log('BRL →→→', BRL);
-  const { rate_float } = dataFetch.USD;
-  const BTCReais = BRL * rate_float;
-  return res.status(200).send({ BTCReais });
-});
+const callBackrequest = async (req, res) => {
+  let data = await getSomeData();
+
+  const { rate_float: rate } = data.bpi.USD;
+  const { BRL: real, CAD: dolCad } = read;
+
+  const BTCReais = rate * parseF(real, 2);
+  const BTCDolCad = rate * parseF(dolCad, 2);
+
+  const BRL = { BRL: creatorObject('BRL', BTCReais, 'Brazilian Real') };
+  const CAD = { CAD: creatorObject('CAD', BTCDolCad, 'Canadian Dollar') };
+
+  Object.assign(data.bpi, BRL);
+  Object.assign(data.bpi, CAD);
+
+  if (data) {
+    return res.status(200).send({ data });
+  }
+  return res.status(400).send({ mensagem: 'Requisição falhou' });
+}
+
+router.get('/cryto/btc', callBackrequest);
 
 module.exports = router;
-
-const value = {
-  time: {
-    updated: 'May 9, 2020 20:16:00 UTC',
-    updatedISO: '2020-05-09T20:16:00+00:00',
-    updateduk: 'May 9, 2020 at 21:16 BST',
-  },
-  disclaimer:
-    'This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org',
-  chartName: 'Bitcoin',
-  bpi: {
-    USD: {
-      code: 'USD',
-      symbol: '&#36;',
-      rate: '9,780.8376',
-      description: 'United States Dollar',
-      rate_float: 9780.8376,
-    },
-    GBP: {
-      code: 'GBP',
-      symbol: '&pound;',
-      rate: '7,883.9615',
-      description: 'British Pound Sterling',
-      rate_float: 7883.9615,
-    },
-    EUR: {
-      code: 'EUR',
-      symbol: '&euro;',
-      rate: '8,915.5855',
-      description: 'Euro',
-      rate_float: 8915.5855,
-    },
-  },
-};
---
+++ service/converter.js
@@ -0,0 +1,44 @@
+function formatNumber(value) {
+  value = convertToFloatNumber(value);
+  return value.formatMoney(4, '.', ',');
+}
+
+//transforma a entrada em número float
+var convertToFloatNumber = function(value) {
+  value = value.toString();
+  if (value.indexOf('.') !== -1 && value.indexOf(',') !== -1) {
+    if (value.indexOf('.') < value.indexOf(',')) {
+      //inglês
+      return parseFloat(value.replace(/,/gi, ''));
+    } else {
+      //português
+      return parseFloat(value.replace(/./gi, '').replace(/,/gi, '.'));
+    }
+  } else {
+    return parseFloat(value);
+  }
+};
+
+//prototype para formatar a saída
+Number.prototype.formatMoney = function(c, d, t) {
+  var n = this,
+    c = isNaN((c = Math.abs(c))) ? 2 : c,
+    d = d == undefined ? '.' : d,
+    t = t == undefined ? ',' : t,
+    s = n < 0 ? '-' : '',
+    i = parseInt((n = Math.abs(+n || 0).toFixed(c))) + '',
+    j = (j = i.length) > 3 ? j % 3 : 0;
+  return (
+    s +
+    (j ? i.substr(0, j) + t : '') +
+    i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + t) +
+    (c
+      ? d +
+        Math.abs(n - i)
+          .toFixed(c)
+          .slice(2)
+      : '')
+  );
+};
+
+module.exports = formatNumber;
