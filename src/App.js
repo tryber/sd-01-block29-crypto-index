04fe859eae8d7abaa5d9adb9d06e9b01afd83dff
Guilherme Luiz <guiiluiz44@gmail.com>
2020-05-26 19:27:31 -0300

    comment components in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Login from './Components/Login';
-import Home from './Components/Home';
+// import Login from './Components/Login';
+// import Home from './Components/Home';
 import UpdateCurrency from './Components/UpdateCurrency';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Login />
-      <Home />
+      {/* <Login /> */}
+      {/* <Home /> */}
       <UpdateCurrency />
     </div>
   );
