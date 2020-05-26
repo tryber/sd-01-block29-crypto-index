d0dbdeb298d899f0894dd5ead0bfd9205592217b
vrramosm <vf.ramos7@gmail.com>
2020-05-26 17:50:14 -0300

    Adicionado o build

---
+++ build/precache-manifest.d16eb51fe88b8166d26696b642c10a2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a8f056186eadd0dafbfa77192e2c01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02d73c703a7f6ffe752d",
+    "url": "/static/css/main.4fff85a1.chunk.css"
+  },
+  {
+    "revision": "1a784fd4c78a1903ce3b",
+    "url": "/static/js/2.7c72c81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c72c81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d73c703a7f6ffe752d",
+    "url": "/static/js/main.1492f702.chunk.js"
+  },
+  {
+    "revision": "dab86807653a19b1bce7",
+    "url": "/static/js/runtime-main.b580dfbe.js"
+  }
+]);
