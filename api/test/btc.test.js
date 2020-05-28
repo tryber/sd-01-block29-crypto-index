e5b1d97aeace209fc591e50d36b6142d88033d3c
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-05-28 18:49:13 -0300

    comitando a retiradas das aspas simples

--- api/test/btc.test.js
+++ api/test/btc.test.js
@@ -1,38 +1,38 @@
-const nock = require("nock");
-const axiosist = require("axiosist");
-const app = require("../server");
-const { validEmail } = require("./fixtures");
+const nock = require('nock');
+const axiosist = require('axiosist');
+const app = require('../server');
+const { validEmail } = require('./fixtures');
 
 jest.setTimeout(100000);
 
-const originalCoinbaseTimeout = `${process.env.COINBASE_API_TIMEOUT || "3000"}`;
+const originalCoinbaseTimeout = `${process.env.COINBASE_API_TIMEOUT || '3000'}`;
 process.env.COINBASE_API_TIMEOUT = 10;
 
-describe("GET /crypto/btc", () => {
+describe('GET /crypto/btc', () => {
   const axios = axiosist(app);
 
   afterEach(() => {
     nock.cleanAll();
   });
 
-  describe("when coinbase API is offline", () => {
+  describe('when coinbase API is offline', () => {
     let response;
 
     beforeAll(async () => {
       const token = await axios
-        .post("/login", validEmail)
+        .post('/login', validEmail)
         .then(({ data }) => data.token);
 
-      nock("https://api.coindesk.com")
-        .defaultReplyHeaders({ "access-control-allow-origin": "*" })
-        .get("/v1/bpi/currentprice.json")
+      nock('https://api.coindesk.com')
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get('/v1/bpi/currentprice.json')
         .delayConnection(1000)
         .reply(200, {});
 
-      response = await axios.get("/crypto/btc", {
+      response = await axios.get('/crypto/btc', {
         headers: {
-          Authorization: token
-        }
+          Authorization: token,
+        },
       });
     });
 
@@ -40,62 +40,62 @@ describe("GET /crypto/btc", () => {
       process.env.COINBASE_API_TIMEOUT = originalCoinbaseTimeout;
     });
 
-    it("returns a 503 HTTP status code", () => {
+    it('returns a 503 HTTP status code', () => {
       expect(response.status).toBe(503);
     });
 
-    it("returns a `coinbase service not available` message", () => {
-      expect(response.data.mensagem).toBe("coinbase service not available");
+    it('returns a `coinbase service not available` message', () => {
+      expect(response.data.mensagem).toBe('coinbase service not available');
     });
   });
 
-  describe("when coinbase API is online", () => {
+  describe('when coinbase API is online', () => {
     let response;
 
     beforeAll(async () => {
       const token = await axios
-        .post("/login", loginFixtures.validData)
+        .post('/login', loginFixtures.validData)
         .then(({ data }) => data.token);
 
-      nock("https://api.coindesk.com")
-        .defaultReplyHeaders({ "access-control-allow-origin": "*" })
-        .get("/v1/bpi/currentprice.json")
+      nock('https://api.coindesk.com')
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get('/v1/bpi/currentprice.json')
         .reply(200, fixtures.success);
 
-      response = await axios.get("/crypto/btc", {
-        headers: { Authorization: token }
+      response = await axios.get('/crypto/btc', {
+        headers: { Authorization: token },
       });
     });
 
-    it("returns 200", () => {
+    it('returns 200', () => {
       expect(response.status).toBe(200);
     });
 
-    it("returns an object", () => {
-      expect(typeof response.data.data).toBe("object");
+    it('returns an object', () => {
+      expect(typeof response.data.data).toBe('object');
     });
 
-    describe("the object", () => {
-      it("has a `bpi` property", () => {
-        expect(response.data.data).toHaveProperty("bpi");
+    describe('the object', () => {
+      it('has a `bpi` property', () => {
+        expect(response.data.data).toHaveProperty('bpi');
       });
 
-      it("has BTC value in BRL", () => {
-        expect(response.data.data.bpi).toHaveProperty("BRL");
-        expect(response.data.data.bpi.BRL).toHaveProperty("rate_float");
-        expect(typeof response.data.data.bpi.BRL.rate_float).toBe("number");
+      it('has BTC value in BRL', () => {
+        expect(response.data.data.bpi).toHaveProperty('BRL');
+        expect(response.data.data.bpi.BRL).toHaveProperty('rate_float');
+        expect(typeof response.data.data.bpi.BRL.rate_float).toBe('number');
       });
 
-      it("has BTC value in USD", () => {
-        expect(response.data.data.bpi).toHaveProperty("USD");
-        expect(response.data.data.bpi.USD).toHaveProperty("rate_float");
-        expect(typeof response.data.data.bpi.USD.rate_float).toBe("number");
+      it('has BTC value in USD', () => {
+        expect(response.data.data.bpi).toHaveProperty('USD');
+        expect(response.data.data.bpi.USD).toHaveProperty('rate_float');
+        expect(typeof response.data.data.bpi.USD.rate_float).toBe('number');
       });
 
-      it("has BTC value in CAD", () => {
-        expect(response.data.data.bpi).toHaveProperty("CAD");
-        expect(response.data.data.bpi.CAD).toHaveProperty("rate_float");
-        expect(typeof response.data.data.bpi.CAD.rate_float).toBe("number");
+      it('has BTC value in CAD', () => {
+        expect(response.data.data.bpi).toHaveProperty('CAD');
+        expect(response.data.data.bpi.CAD).toHaveProperty('rate_float');
+        expect(typeof response.data.data.bpi.CAD.rate_float).toBe('number');
       });
     });
   });
--- api/test/fixtures.js
+++ api/test/fixtures.js
@@ -1,4 +1,4 @@
-const validEmail = { email: "doug@gmail.com", password: "123456" };
-const invalidEmail = { email: "teste.teste.com", password: "123456" };
+const validEmail = { email: 'doug@gmail.com', password: '123456' };
+const invalidEmail = { email: 'teste.teste.com', password: '123456' };
 
 module.exports = { validEmail, invalidEmail };
