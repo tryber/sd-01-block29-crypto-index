7d07d7c8c60c4a285878870354a5ac07fa62527d
vrramosm <vf.ramos7@gmail.com>
2020-05-29 20:01:44 -0300

    Criado os testes para a rota POST /crypto/btc

--- api/tests/routes/crypto-btc.test.js
+++ api/tests/routes/crypto-btc.test.js
@@ -19,15 +19,15 @@ describe('GET /crypto/btc', () => {
   describe('when coinbase is offline', () => {
     let response;
 
-    beforeAll( async () => {
+    beforeAll(async () => {
       const token = await axios.post('/login', loginFixtures.validLogin)
-      .then(({ data }) => data.token); 
-    
+        .then(({ data }) => data.token);
+
       nock('https://api.coindesk.com')
-      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-      .get('/v1/bpi/currentprice/BTC.json')
-      .delayConnection(1000)
-      .reply(200, {});
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get('/v1/bpi/currentprice/BTC.json')
+        .delayConnection(1000)
+        .reply(200, {});
 
       response = await axios.get('/crypto/btc', { headers: { Authorization: token } });
     });
@@ -48,14 +48,14 @@ describe('GET /crypto/btc', () => {
   describe('when coinbase is online', () => {
     let response;
 
-    beforeAll( async () => {
+    beforeAll(async () => {
       const token = await axios.post('/login', loginFixtures.validLogin)
-      .then(({ data }) => data.token);
+        .then(({ data }) => data.token);
 
       nock('https://api.coindesk.com')
-      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-      .get('/v1/bpi/currentprice/BTC.json')
-      .reply(200, btcFixtures.success);
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get('/v1/bpi/currentprice/BTC.json')
+        .reply(200, btcFixtures.success);
 
       response = await axios.get('/crypto/btc', { headers: { Authorization: token } });
     });
@@ -66,6 +66,87 @@ describe('GET /crypto/btc', () => {
 
     it('returns an object', () => {
       expect(typeof response.data).toBe('object');
+    });
+  });
+});
+
+describe('POST /crypto/btc', () => {
+  const axios = axiosist(app);
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  describe('when token is invalid', () => {
+    let response;
+
+    beforeAll(async () => {
+      response = await axios.post('/crypto/btc', { currency: 'BRL', value: 10 }, { headers: { Authorization: 9875 } });
+    });
+
+    it('returns a 401 HTTP status code', () => {
+      expect(response.status).toBe(401);
+    });
+
+    it('returns a `Token Inválido` error message.', () => {
+      expect(response.data.message).toBe('Token Inválido');
+    });
+  });
+
+  describe('when currency is invalid', () => {
+    let response;
+
+    beforeAll(async () => {
+      const token = await axios.post('/login', loginFixtures.validLogin)
+        .then(({ data }) => data.token);
+
+      response = await axios.post('/crypto/btc', { currency: 'BL', value: 10 }, { headers: { Authorization: token } });
+    });
+
+    it('returns a 400 HTTP error', () => {
+      expect(response.status).toBe(400);
+    });
+
+    it('returns `Moeda Inválida` error message', () => {
+      expect(response.data.message).toBe('Moeda Inválida');
+    });
+  });
+
+  describe('when value is invalid', () => {
+    let response;
+
+    beforeAll(async () => {
+      const token = await axios.post('/login', loginFixtures.validLogin)
+        .then(({ data }) => data.token);
+
+      response = await axios.post('/crypto/btc', { currency: 'BRL', value: -10 }, { headers: { Authorization: token } });
+    });
+
+    it('return a 400 HTTP error', () => {
+      expect(response.status).toBe(400);
+    });
+
+    it('return `Valor Inválido` error message', () => {
+      expect(response.data.message).toBe('Valor Inválido');
+    });
+  });
+
+  describe('when everything is ok', () => {
+    let response;
+
+    beforeAll(async () => {
+      const token = await axios.post('/login', loginFixtures.validLogin)
+        .then(({ data }) => data.token);
+
+      response = await axios.post('/crypto/btc', { currency: 'BRL', value: 5200 }, { headers: { Authorization: token } });
+    });
+
+    it('returns 200', () => {
+      expect(response.status).toBe(200);
+    });
+
+    it('return a `Valor alterado com sucesso!` message', () => {
+      expect(response.data.message).toBe('Valor alterado com sucesso!');
     });
   });
 });
