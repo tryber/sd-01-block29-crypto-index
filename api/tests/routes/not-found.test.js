12d9926209c287caa6faec345f165a2e15a0ef2a
vrramosm <vf.ramos7@gmail.com>
2020-05-29 17:06:04 -0300

    Criado o teste para urls nao existentes

--- api/tests/routes/not-found.test.js
+++ api/tests/routes/not-found.test.js
@@ -1,4 +1,24 @@
 const axiosist = require("axiosist");
-const app = require(app);
+const app = require('../../server');
 
-const axios = axiosist(app);
+jest.setTimeout(10000);
+
+describe('GET or POST Not Found', () => {
+  const axios = axiosist(app);
+
+  describe('when a url does not exist', () => {
+    let response;
+
+    beforeAll(async () => {
+      response = await axios.post('/lll', {});
+    });
+
+    it('returns a 404 HTTP status code', () => {
+      expect(response.status).toBe(404);
+    });
+
+    it('returns a `Endpoint não encontrado.` error message', () => {
+      expect(response.data.message).toBe('Endpoint não encontrado.');
+    });
+  });
+});
