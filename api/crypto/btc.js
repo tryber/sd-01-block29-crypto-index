2703e298897f305e5deec695df69b4946a48df3d
Conrado Medeiros <conrado.medeiros@localiza.com>
2020-06-05 19:33:59 -0300

    cc

--- api/crypto/btc.js
+++ api/crypto/btc.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const axiosFetch = require("./service");
-const fs = require("fs").promises;
-const path = require("path");
-const validate = require("./middlewarebtc");
-const rescue = require("../rescue");
+const express = require('express');
+const axiosFetch = require('./service');
+const fs = require('fs').promises;
+const path = require('path');
+const validate = require('./middlewarebtc');
+const rescue = require('../rescue');
 
 const router = express.Router();
 
-const url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json";
+const url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json';
 
 const fileModifier = async (fileModifierType, newContent) => {
-  const filePath = path.resolve(__dirname, "..", "..", "currencies.json");
+  const filePath = path.resolve(__dirname, '..', '..', 'currencies.json');
   const readFile = () =>
     fs.readFile(filePath).then(fileContent => JSON.parse(fileContent));
   const writeFile = content =>
@@ -18,7 +18,7 @@ const fileModifier = async (fileModifierType, newContent) => {
   const choices = {
     read: readFile,
     write: writeFile,
-    default: "Tipo de modificador errado",
+    default: 'Tipo de modificador errado',
   };
   return choices[fileModifierType](newContent) || choices.default;
 };
@@ -41,15 +41,15 @@ const coinObj = (data, coin, description, value) => {
 };
 
 const callbackGetBTC = async (req, res) => {
-  const data = await axiosFetch(url).catch((err) => ({ error: err }));
-  const read = await fileModifier("read");
+  const data = await axiosFetch(url).catch(err => ({ error: err }));
+  const read = await fileModifier('read');
   if ((data.error && !data.error.response) || !data)
-    return res.status(503).json({ message: "Coinbase service not available" });
-  if (data.error) return res.status(500).json({ message: "Error no data" });
+    return res.status(503).json({ message: 'Coinbase service not available' });
+  if (data.error) return res.status(500).json({ message: 'Error no data' });
   const { BRL, CAD, EUR } = read;
-  const objReal = coinObj(data.bpi.USD, "BRL", "Brazilian Real", BRL);
-  const objCad = coinObj(data.bpi.USD, "CAD", "Canadian Dollar", CAD);
-  const objEur = coinObj(data.bpi.USD, "EUR", "Euro", EUR);
+  const objReal = coinObj(data.bpi.USD, 'BRL', 'Brazilian Real', BRL);
+  const objCad = coinObj(data.bpi.USD, 'CAD', 'Canadian Dollar', CAD);
+  const objEur = coinObj(data.bpi.USD, 'EUR', 'Euro', EUR);
   Object.assign(data.bpi, objReal);
   Object.assign(data.bpi, objEur);
   Object.assign(data.bpi, objCad);
@@ -59,17 +59,17 @@ const callbackGetBTC = async (req, res) => {
 const callbackPostCoin = async (req, res) => {
   const { currency, value } = req.body;
   try {
-    const sucess = { message: "Valor alterado com sucesso!" };
-    const read = await fileModifier("read");
+    const sucess = { message: 'Valor alterado com sucesso!' };
+    const read = await fileModifier('read');
     read[currency] = value;
-    await fileModifier("write", read);
+    await fileModifier('write', read);
     return res.status(200).json(sucess);
   } catch (error) {
     console.error();
   }
 };
 
-router.post("/crypto/btc", validate, rescue(callbackPostCoin));
-router.get("/crypto/btc", rescue(callbackGetBTC));
+router.post('/crypto/btc', validate, rescue(callbackPostCoin));
+router.get('/crypto/btc', rescue(callbackGetBTC));
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const login = require("./login");
-const btc = require("./crypto/btc");
+const express = require('express');
+const cors = require('cors');
+const login = require('./login');
+const btc = require('./crypto/btc');
 
 const app = express();
 
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use(login.router);
 app.use(btc);
-app.use("*", (req, res) =>
-  res.status(404).json({ message: "Endpoint não encontrado" })
+app.use('*', (req, res) =>
+  res.status(404).json({ message: 'Endpoint não encontrado' })
 );
 app.use((error, _req, res, _next) => {
   res.status(500).json({ message: error.message, stack: error.stack });
